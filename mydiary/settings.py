eaf2a7dd73e60b8a9a734ad3a651b6a744be3d4e
adityababar07 <adityababar715@gmail.com>
2021-08-07 15:27:19 +0530

    added hosts

--- mydiary/settings.py
+++ mydiary/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'chj6*b%la6$w!)&pquxgyz&he49*3x^2^0vn7xb482%^2xfxlg'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*', '127.0.0.1']
 
 
 # Application definition
@@ -74,9 +74,20 @@ WSGI_APPLICATION = 'mydiary.wsgi.application'
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
 DATABASES = {
+    'default':{
+        'ENGINE'   : 'django.db.backends.postgresql',
+        'NAME'     : 'mydiary',
+        'USER'     : 'hacker07',
+        'PASSWORD' : 'mysecurepass@1234',
+        'HOST'     : 'localhost',
+        'PORT'     : '5432'
+    }
+}
+
+DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME'  : os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
